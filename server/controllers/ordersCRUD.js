c6143955b2033afb9e16162863e132218608274e
j499412@gmail.com <j499412@gmail.com>
2026-08-10 17:34:03 +0300

    removed trailing order

--- server/controllers/ordersCRUD.js
+++ server/controllers/ordersCRUD.js
@@ -337,7 +337,7 @@ const payOrder = async (req, res) => {
                 billing_city,
                 billing_state,
                 billing_zip,
-                billing_country,
+                billing_country
             )
             VALUES
             ( $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21 )
